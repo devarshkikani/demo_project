c75ee4da8ce8b831c9a13fe7490c0c8e52cfd5ae
Devarsh Kikani <devarshkikani2904@gmail.com>
2025-03-05 14:45:11 +0530

    initial commit

---
+++ cmd/students-api/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var name = "Hello"
+	var secondName string = "DOSto!!"
+
+	fmt.Println(name + secondName)
+
+}
+
---
+++ go.mod
@@ -0,0 +1,3 @@
+module cmd/students-api
+
+go 1.23.6
